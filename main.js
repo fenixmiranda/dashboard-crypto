1b9a1d256a756a6e60542d6009c0c04a77b2f9ef
fenixmiranda <fenixacmiranda@gmail.com>
2022-12-08 18:50:13 -0300

    inclusão da opção do tema dark

--- main.js
+++ main.js
@@ -27,3 +27,14 @@ new Chart(chart, {
         responsive: true
     }
 })
+
+// Alteração de tema
+
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
