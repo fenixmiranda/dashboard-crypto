96e12037d1f75a27c48861d3207a5ba2799de4ab
fenixmiranda <fenixacmiranda@gmail.com>
2022-12-10 11:21:48 -0300

    responsividade sidebar

--- main.js
+++ main.js
@@ -38,3 +38,16 @@ themeBtn.addEventListener('click', () => {
     themeBtn.querySelector('span:first-child').classList.toggle('active');
     themeBtn.querySelector('span:last-child').classList.toggle('active');
 })
+
+// show or hide sidebar 
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+})
